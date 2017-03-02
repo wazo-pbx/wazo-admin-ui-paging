d49d3c735688246e5405f9e6ac2a8a11c59b9a72
Sylvain Boily <sylvainboiydroid@gmail.com>
2017-03-02 16:18:45 -0500

    add and get features

--- paging/service.py
+++ paging/service.py
@@ -9,3 +9,27 @@ class PagingService(BaseConfdService):
 
     resource = 'paging'
     confd_resource = 'pagings'
+
+    def create(self, resources):
+        paging = resources.get(self.resource)
+        members = paging.get('members')
+        callers = paging.get('callers')
+
+        paging = self._confd.pagings.create(paging)
+
+        if members:
+            self.add_members_to_paging(paging, self._generate_users(members))
+        if callers:
+            self.add_callers_to_paging(paging, self._generate_users(callers))
+
+    def get_users(self):
+        return self._confd.users.list()
+
+    def add_members_to_paging(self, paging, members):
+        return self._confd.pagings.relations(paging).update_user_members(members)
+
+    def add_callers_to_paging(self, paging, callers):
+        return self._confd.pagings.relations(paging).update_user_callers(callers)
+
+    def _generate_users(self, users):
+        return [{'uuid': user } for user in users]
--- paging/view.py
+++ paging/view.py
@@ -36,3 +36,22 @@ class PagingView(BaseView):
     @classy_menu_item('.pagings', 'Pagings', order=4, icon="bullhorn")
     def index(self):
         return super(PagingView, self).index()
+
+    def _populate_form(self, form):
+        users = self.service.get_users()
+        users = self._user_list(users['items'])
+        form.members.choices = users
+        form.callers.choices = users
+        return form
+
+    def _map_resources_to_form(self, resources):
+        members = self._get_user(resources['paging']['members']['users'])
+        callers = self._get_user(resources['paging']['callers']['users'])
+        return self.form(data=resources['paging'], members=members, callers=callers)
+
+    def _user_list(self, users):
+        return [(user['uuid'], u"{} {}".format(user['firstname'], user['lastname']))
+                 for user in users]
+
+    def _get_user(self, users):
+        return [user['uuid'] for user in users]

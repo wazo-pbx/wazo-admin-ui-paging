384b2569d0911cef28c1d99af4ec7e4f16a5fc1b
Sylvain Boily <sylvainboiydroid@gmail.com>
2017-03-23 21:42:08 -0400

    add update for members and callers

--- paging/service.py
+++ paging/service.py
@@ -11,24 +11,33 @@ class PagingService(BaseConfdService):
     resource_confd = 'pagings'
 
     def create(self, resources):
+        resource = super(PagingService, self).create(resources)
+        self._update_members_callers(resources, resource)
+
+    def update(self, resources):
+        super(PagingService, self).update(resources)
+        self._update_members_callers(resources)
+
+    def _update_members_callers(self, resources, resource=None):
         paging = resources.get(self.resource_name)
         members = paging.get('members')
         callers = paging.get('callers')
 
-        paging = self._confd.pagings.create(paging)
+        if resource == None:
+            resource = paging['id']
 
         if members:
-            self.add_members_to_paging(paging, self._generate_users(members))
+            self._update_members_to_paging(resource, self._generate_users(members))
         if callers:
-            self.add_callers_to_paging(paging, self._generate_users(callers))
+            self._update_callers_to_paging(resource, self._generate_users(callers))
 
     def get_users(self):
         return self._confd.users.list()
 
-    def add_members_to_paging(self, paging, members):
+    def _update_members_to_paging(self, paging, members):
         return self._confd.pagings.relations(paging).update_user_members(members)
 
-    def add_callers_to_paging(self, paging, callers):
+    def _update_callers_to_paging(self, paging, callers):
         return self._confd.pagings.relations(paging).update_user_callers(callers)
 
     def _generate_users(self, users):

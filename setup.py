d2f1a8f86c448163cd2c4daebb922aa181f615a4
Cedric Abunar <cabunar@wazo.io>
2018-02-26 13:41:22 -0500

    fix python project name with - instead of _

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
 
-PROJECT = 'wazo-admin-ui-paging'
+PROJECT = 'wazo_admin_ui_paging'
 AUTHOR = 'Wazo Authors'
 EMAIL = 'dev@wazo.community'
 
